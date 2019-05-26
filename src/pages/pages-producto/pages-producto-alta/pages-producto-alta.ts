9154d3edfe0b4b3130d0af59c9764138eb1f4d08
Nicolas Saavedra Rojas <njsr_27@hotmail.com>
2019-05-26 00:06:54 -0300

    Arreglos generales, interface de form producto creada

--- src/pages/pages-producto/pages-producto-alta/pages-producto-alta.ts
+++ src/pages/pages-producto/pages-producto-alta/pages-producto-alta.ts
@@ -8,11 +8,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProductoAltaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  producto = {}
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductoAltaPage');
+  cargar() {
+    console.log(this.producto)
   }
 
 }
