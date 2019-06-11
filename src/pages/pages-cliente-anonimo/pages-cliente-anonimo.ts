da3c44d4a83dc2f10534b694e02bd7fb1ef7f746
DamianVogel <rodolfovogel@gmail.com>
2019-06-10 22:30:39 -0300

    Se guardan clientes anonimos con Id, agregado de spinner y deshabilitacion de boton mientras se realiza el request.

--- src/pages/pages-cliente-anonimo/pages-cliente-anonimo.ts
+++ src/pages/pages-cliente-anonimo/pages-cliente-anonimo.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ModalController } from 'ionic-angular';
 import { Usuario } from '../../clases/usuario';
 import { PagesClienteAnonimoMenuPage } from './pages-cliente-anonimo-menu/pages-cliente-anonimo-menu';
-import { showAlert } from '../../environments/environment';
+import { showAlert, spin } from '../../environments/environment';
 import { UsuarioService } from '../../services/usuario-service';
 
 @IonicPage()
@@ -12,24 +12,55 @@ import { UsuarioService } from '../../services/usuario-service';
 })
 export class PagesClienteAnonimoPage {
 
+  botonHabilitado:boolean = true;
+
   usuario: Usuario;
   usuarios: Usuario[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertController: AlertController, public usuarioService: UsuarioService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public alertController: AlertController, 
+    public usuarioService: UsuarioService,
+    public modalCtrl: ModalController,
+    
+    
+    ) {
     this.usuario = new Usuario;
     this.usuarioService.traerUsuarios().subscribe(usuarios => {
       this.usuarios = usuarios;
     });;
   }
 
-  acceder() {
+  acceder() {    
+    
+    
     if(this.usuario.nombre === undefined || this.usuario.nombre === ""){
       showAlert(this.alertController,"Error","Debe ingresar un nombre");
     } else if(this.usuarioService.validarUsuarioExiste(this.usuarios, this.usuario.nombre)){
       showAlert(this.alertController,"Error","Ya existe un usuario con ese nombre en el sistema");
     } else {
-      sessionStorage.setItem("usuario", JSON.stringify(this.usuario));
-      this.navCtrl.push(PagesClienteAnonimoMenuPage);
+      
+      spin(this.modalCtrl, true);
+      this.botonHabilitado = false;      
+      let usuarioAnonimo =  new Usuario();
+     
+
+      usuarioAnonimo.nombre = this.usuario.nombre;
+      usuarioAnonimo.perfil = 'anonimo';
+      this.usuarioService.cargarUsuario(usuarioAnonimo.dameJSON()).then( alta => {
+        this.botonHabilitado = true;
+        spin(this.modalCtrl, false);
+        
+        sessionStorage.setItem("usuario", JSON.stringify(this.usuario));
+        this.navCtrl.push(PagesClienteAnonimoMenuPage);
+      }).catch( error =>{
+        
+        console.log(error);
+        this.botonHabilitado = true;
+        spin(this.modalCtrl, false);
+      })
+       
     }
   }
 
