0f88b8c2ffde590e872681eeaf9924ac9dade8e5
DamianVogel <rodolfovogel@gmail.com>
2019-06-26 12:27:59 -0300

    Se suma grafico de encuesta ingreso empleado.

--- src/pages/pages-dueno/pages-dueno-menu/pages-dueno-menu.ts
+++ src/pages/pages-dueno/pages-dueno-menu/pages-dueno-menu.ts
@@ -7,6 +7,10 @@ import { MesasProvider } from '../../../providers/mesas/mesas';
 import { PagesReservasPage } from '../../pages-reservas/pages-reservas';
 import { PagesRegistrosPendientesPage } from '../../pages-registros-pendientes/pages-registros-pendientes';
 import { SoundsService } from '../../../services/sounds-service';
+import { Encuesta_empleado } from '../../../clases/Encuesta_empleado';
+import { Chart } from 'chart.js';
+import { showAlert, spin, getRandomColor, round } from '../../../environments/environment';
+import { EncuestaService } from '../../../services/encuesta-service';
 
 
 @IonicPage()
@@ -15,6 +19,13 @@ import { SoundsService } from '../../../services/sounds-service';
   templateUrl: 'pages-dueno-menu.html',
 })
 export class PagesDuenoMenuPage {
+
+  graficoMostrado: boolean = false;
+  doughnutChart: any;
+  encuestas: Encuesta_empleado[];
+  encuestasCargadas: boolean;
+  
+  @ViewChild('doughnutCanvas') doughnutCanvas;
 
   @ViewChild(Navbar) navBar: Navbar;
 
@@ -41,9 +52,10 @@ export class PagesDuenoMenuPage {
     public modalVotacion: ModalController,
     private mesasProvider: MesasProvider,
     public actionSheetController: ActionSheetController,
-    private soundsService: SoundsService
+    private soundsService: SoundsService,
+    public encuestaService: EncuestaService,
   ) {
-
+    this.cargarEncuestas();
   }
 
   ionViewDidLoad(){
@@ -95,6 +107,89 @@ export class PagesDuenoMenuPage {
     this.navCtrl.push(this.registrosPendiente);
   }
 
+  mostrarGrafico() {
+    if (this.graficoMostrado) {
+      this.graficoMostrado = false;
+      document.getElementById('grafico').style.display = 'none';
+    } else {
+      this.graficoMostrado = true;
+      document.getElementById('grafico').style.display = 'block';
+      this.cargarGrafico();
+    }
+  }
+
+  cargarGrafico() {
+
+    let encuestasAGraficar: Array<Encuesta_empleado>;
+
+    encuestasAGraficar =  this.encuestas;
+    // if (this.usuario.perfil === "empleado") {
+    //   encuestasAGraficar = this.encuestas.filter(encuesta => {
+    //     return encuesta.empleado === this.usuario.nombre;
+    //   });
+    // } else if (this.usuario.perfil === "cliente") {
+    //   encuestasAGraficar = this.encuestas.filter(encuesta => {
+    //     return encuesta.cliente === this.usuario.nombre;
+    //   });
+    // }
+
+    //encuestasAGraficar = this.cargarMockData(encuestasAGraficar); //Comentar para testear
+
+    var graphLabels = ["estadoPuestoTrabajo", "estadoBanos", "estadoSalon", "basuraPisos",  "limpiaMesas"];
+    var data = [];
+    var colors = [];
+
+    //Por cada atributo, saco los numeros de todas las encuestas, y de la suma de todos esos numeros, saco el promedio.
+    //Si el atributo es basuraPiso/usaBienBaño/limpiaMesa, que son checkbox, solamente los sumo, no saco el promedio
+    if (encuestasAGraficar.length !== 0) {
+      graphLabels.forEach(function (label) {
+        if (label === "basuraPisos" || label === "limpiaMesas") {
+          data.push(round((encuestasAGraficar.map(encuesta => { return encuesta[label] }).reduce(function (total, sum) { return total + sum })), 2))
+        } else {
+          data.push(round((encuestasAGraficar.map(encuesta => { return encuesta[label] }).reduce(function (total, sum) { return total + sum })) / encuestasAGraficar.length, 2))
+        }
+        colors.push(getRandomColor());
+      });
+    }
+    graphLabels[graphLabels.indexOf("estadoPuestoTrabajo")] = "Estado del puesto de trabajo";    
+    graphLabels[graphLabels.indexOf("estadoBanos")] = "Estado del baño";
+    graphLabels[graphLabels.indexOf("estadoSalon")] = "Estado del salon";
+    graphLabels[graphLabels.indexOf("basuraPisos")] = "Pisos limpios";  
+    graphLabels[graphLabels.indexOf("limpiaMesas")] = "Mesas limpias";
+    
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+     
+      type: 'doughnut',
+      data: {
+        labels: graphLabels,
+        datasets: [{
+          label: '# promedio de calificacion', 
+          data: data,
+          backgroundColor: colors
+        }]
+      },
+      options: {
+        legend: {
+          display: true
+        }
+      }
+
+    });
+  }
+
+  cargarEncuestas() {
+    this.encuestaService.traerEncuestasEmpleados().subscribe(encuestas => {
+      this.encuestas = encuestas;
+      this.validarEncuestaCargada();
+    })
+  }
+
+  validarEncuestaCargada() {
+    
+      if (this.encuestas.length > 0) {
+        this.encuestasCargadas = true;
+      };
+  }
 
 }
 
